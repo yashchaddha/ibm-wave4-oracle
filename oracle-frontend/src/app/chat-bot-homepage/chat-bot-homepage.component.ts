2464524ddd7926d9164219fa69451e167128635e
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2019-03-30 22:19:34 +0530

    improve chat ui

--- oracle-frontend/src/app/chat-bot-homepage/chat-bot-homepage.component.ts
+++ oracle-frontend/src/app/chat-bot-homepage/chat-bot-homepage.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, QueryList } from "@angular/core";
+
+interface Status {
+  accepted: boolean;
+  answered: boolean;
+}
+interface Query {
+  question: string;
+  answer: string;
+}
+interface UserQuery {
+  query: Query;
+  status: Status;
+}
 
 @Component({
   selector: "app-chat-bot-homepage",
@@ -7,10 +20,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ChatBotHomepageComponent implements OnInit {
   constructor() {}
-  query: string = "";
+
+  query: Query = {
+    question: "",
+    answer: ""
+  };
+  status: Status = {
+    accepted: false,
+    answered: false
+  };
+
+  queryList: any = [];
 
   ngOnInit() {}
   onSubmit(value) {
-    console.log("submitted" + JSON.stringify(value));
+    let jsonQuery = JSON.stringify({ query: this.query, status: this.status });
+    console.log("submitted" + jsonQuery);
+    this.queryList.push(JSON.parse(jsonQuery));
+    console.log(this.queryList);
+    value.reset();
   }
 }
