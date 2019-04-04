b81e65bd181e2f47860a4df93eaac489a095e17f
gattinenirohini66 <gattinenirohini66>
2019-04-04 09:36:57 +0530

    resolved issues

--- oracle-frontend/src/environments/environment.prod.ts
+++ oracle-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   saveData:'http://localhost:8081/api/v1/saveUser',
-  loginData:'http://localhost:8081/login',
-  allUserData:'http://localhost:8081/users',
-  singleUserData:'http://localhost:8081/getUser'
+  loginData:'http://localhost:8081/api/v1/login',
+  allUserData:'http://localhost:8081/api/v1/users',
+  singleUserData:'http://localhost:8081/api/v1/User'
 };
